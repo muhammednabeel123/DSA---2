4e2c7cbc911976ab80520bf08a31866622a896e4
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-20 02:06:43 +0530

    first commit

--- BubbleSort.js
+++ BubbleSort.js
@@ -13,6 +13,29 @@ function BubbleSort(arr){
    }while(swapped)
 }
 
-const arr = [8,20,-2,4,-6]
-BubbleSort(arr)
+
+// optmized code
+
+
+function BubbleSort2(arr){
+    for(let i=0 ; i<arr.length-1 ; i++){
+       let flag = 0
+       for(let j=0;j<arr.length-1-i;j++){
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] =temp
+            flag = 1
+        }
+       } 
+      
+       if(flag == 0){
+        break;
+       }
+    }
+ 
+}
+
+const arr = [8,20,2,4,6]
+BubbleSort2(arr)
 console.log(arr)
