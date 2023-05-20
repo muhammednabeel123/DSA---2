c2f418c2c147c02aaa338b4702e5cd3fb8626011
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-21 03:55:43 +0530

    selection sort aded

--- quickSort.js
+++ quickSort.js
@@ -36,3 +36,31 @@ function Partition(arr, lb, ub) {
   quickSort(arr, 0, arr.length - 1);
   console.log(arr);
   
+
+  //simpler method 
+
+  function quickSort2(arr){
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+   for(let i =0 ;i<arr.length-1 ;i++){
+    if(arr[i] < pivot){
+        left.push(arr[i])
+    
+
+    }
+    else{
+        right.push(arr[i])
+    }
+   }
+   return [...quickSort2(left),pivot,...quickSort2(right)  ]
+
+
+
+  }
+
+  let arr1 = [9, 3, 7, 1, 5];
+  console.log(quickSort2(arr1));
