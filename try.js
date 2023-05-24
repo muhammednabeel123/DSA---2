153b59e5ea4cfe4656c4993b0f1b77c7dee2e6b0
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-25 03:49:42 +0530

    changes added

--- try.js
+++ try.js
@@ -1,32 +1,160 @@
-class stack {
-    constructor(){
-        this.item = []
-    }
+// class stack {
+//     constructor(){
+//         this.item = []
+//     }
 
-    push(element){
-        this.item.push(element) 
-    }
+//     push(element){
+//         this.item.push(element) 
+//     }
 
-    pop(){
-       return this.item.pop()
-    }
+//     pop(){
+//        return this.item.pop()
+//     }
 
-    peek(){
-        return this.item[this.item.length-1]
-    }
+//     peek(){
+//         return this.item[this.item.length-1]
+//     }
 
-    print(){
-        console.log(this.item.toString());
-    }
+//     print(){
+//         console.log(this.item.toString());
+//     }
     
-}
+// }
 
 
 
-const item =  new stack()
+// const item =  new stack()
 
-item.push(20)
-item.push(30)
-console.log(item.peek());
+// item.push(20)
+// item.push(30)
+// console.log(item.peek());
 
-item.print()
+// item.print()
+
+// class queue {
+//     constructor(){
+//         this.item = {}
+//         this.rear = 0
+//         this.front = 0
+//     }
+
+//     enqueue(element){
+//         this.item[this.rear] = element
+//         this.rear ++
+
+//     }
+
+//     dequeue(){
+//         const item = this.item[this.front]
+//         delete this.item[this.front]
+//         this.front ++
+//         return item
+//     }
+
+//     isEmpty(){
+//        return this.rear - this.front === 0
+//     }
+
+//     peak(){
+//         return this.item[this.front]
+//     }
+
+//     print(){
+//         console.log(this.item);
+//     }
+
+
+
+
+// }
+
+// const list = new queue()
+
+// list.enqueue(20)
+// list.enqueue(50)
+// list.enqueue(40)
+// list.dequeue()
+// console.log(list.peak());
+// list.print()
+
+
+
+
+class circularQueue {
+    constructor(capacity) {
+      this.items = new Array(capacity);
+      this.capacity = capacity;
+      this.currentLength = 0;
+      this.rear = -1;
+      this.front = -1;
+    }
+  
+    isFull() {
+      return this.currentLength === this.capacity;
+    }
+  
+    isEmpty() {
+      return this.currentLength === 0;
+    }
+  
+    enqueue(element) {
+      if (!this.isFull()) {
+        this.rear = (this.rear + 1) % this.capacity;
+        this.items[this.rear] = element;
+        this.currentLength += 1;
+        if (this.front === -1) {
+          this.front = this.rear;
+        }
+      }
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) {
+        return null;
+      }
+  
+      const item = this.items[this.front];
+      this.items[this.front] = null;
+      this.front = (this.front + 1) % this.capacity;
+      this.currentLength -= 1;
+      if (this.isEmpty()) {
+        this.front = -1;
+        this.rear = -1;
+      }
+  
+      return item;
+    }
+  
+    peek() {
+      if (!this.isEmpty()) {
+        return this.items[this.front];
+      }
+      return null;
+    }
+  
+    print() {
+      if (this.isEmpty()) {
+        console.log('Queue is empty');
+      } else {
+        let i = this.front;
+        let str = '';
+        while (i !== this.rear) {
+          str += this.items[i] + ' ';
+          i = (i + 1) % this.capacity;
+        }
+        str += this.items[i];
+        console.log(str);
+      }
+    }
+  }
+  
+  const queue = new circularQueue(5);
+  
+  queue.enqueue(10);
+  queue.enqueue(20);
+  queue.enqueue(30);
+  queue.enqueue(40);
+  queue.enqueue(50);
+  
+  queue.print();
+  
---
+++ try2.js
@@ -0,0 +1,126 @@
+class circularQueue {
+    constructor(capacity){
+
+        this.item = new Array(capacity)
+        this.capacity = capacity
+        this.rear = -1
+        this.front = -1
+        this.currentLength = 0
+
+    }
+    isFull(){
+        this.capacity === this.currentLength
+    }
+
+    isEmpty(){
+        this.currentLength === 0
+    }
+
+    enqueue(element){
+        
+        
+        if(!this.isFull()){
+            this.rear = (this.rear + 1) % this.capacity
+         
+            this.item[this.rear] = element
+            this.currentLength += 1 
+
+            if(this.front === -1){
+                this.front = this.rear
+            }
+
+
+        }
+
+    }
+
+    dequeue(){
+        if(this.isEmpty()){
+            return  null
+        }
+        this.item[this.front] = null
+
+        this.front = (this.front + 1) % this.capacity
+      
+    if(this.isEmpty()){
+        this.front = -1
+        this.end = -1
+    }
+
+   
+    }
+
+
+    print() {
+        let str = '';
+        let i = this.front;
+        while (i !== this.rear) {
+          str += this.item[i] + ' ';
+          i = (i + 1) % this.capacity;
+        }
+      
+        if (this.item[i] !== undefined) {
+          str += this.item[i];
+        }
+        console.log(str);
+      }
+      
+   
+
+}
+
+
+const queue = new circularQueue(5);
+  
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// queue.enqueue(40);
+// queue.dequeue(50);
+
+// queue.print();
+
+
+function BubbleSort(arr){
+
+    for(let i = 0 ; i < arr.length-1; i++){
+        let flag = 0
+        for(let j = 0 ; j < arr.length-1-i ;j++ ){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                flag = 1
+
+            }
+        }
+        if(flag == 0 ){
+            break;
+        }
+    }
+
+}
+
+// let arr = [100000,32,4454,212,3213,44321]
+// BubbleSort(arr)
+// console.log(arr);
+
+
+function insertion(a){
+    for(let i = 1; i < a.length ;i++ ){
+        temp = a[i]
+        let j = i-1
+
+        while(j >= 0 && a[j] > temp ){
+            a[j + 1] = a[ j ]
+            j--
+        }
+
+        a[j + 1] = temp
+    }
+}
+
+let arr = [100000,32,4454,212,3213,44321]
+insertion(arr)
+console.log(arr);
+
